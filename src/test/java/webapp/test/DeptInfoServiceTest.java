ce43eea6ba6bd97bc5e66df825225bba04a59ad6
usna <nus963@google.com>
2015-03-04 16:32:17 +0900

    developer 1

---
+++ src/test/java/webapp/test/DeptInfoServiceTest.java
@@ -0,0 +1,71 @@
+package webapp.test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import webapp.Util.EmployeeDataSource;
+import webapp.exception.DeptNotFoundException;
+import webapp.model.Dept;
+import webapp.model.Emp;
+import webapp.service.DeptInfoService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/webapp/resource/beans.xml")
+public class DeptInfoServiceTest {
+
+	static Logger log = Logger.getLogger(DeptInfoServiceTest.class);
+	@Autowired
+	ApplicationContext factory;
+
+	@Test
+	public void testGetDeptInfo() {
+		log.info("### testGetDeptInfo");
+
+		DeptInfoService service = factory.getBean(DeptInfoService.class);
+
+		Dept dept = service.getDeptInfo(10);
+		try {
+			assertNotNull(dept);
+
+			log.info("deptno = " + dept.getDeptno());
+			log.info("dname = " + dept.getDname());
+			log.info("loc = " + dept.getLoc());
+		} catch (DeptNotFoundException e) {
+			log.info("dept not found");
+		}
+
+	}
+
+	@Test
+	public void tsetGetDeptInfoWithEmps() {
+		log.info("###@@@ tsetGetDeptInfoWithEmps");
+
+		DeptInfoService service = factory.getBean(DeptInfoService.class);
+
+		Dept dept = service.getDeptInfoWithEmps(10);
+		assertNotNull(dept);
+
+		List<Emp> emps = dept.getEmps();
+		if (emps != null) {
+			for (Emp e : emps) {
+				log.info(e.getEmpno() + " " + e.getEname());
+			}
+		}
+
+		log.info("##################################");
+	}
+
+}
---
+++ src/test/java/webapp/test/PropertiesTest.java
@@ -0,0 +1,33 @@
+package webapp.test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesTest {
+
+	public static void main(String[] args) throws IOException {//예외는 테스트이기 때문에 그냥 던짐 원래는 다 잡아야된다
+
+		Properties dbprop = new Properties();
+		
+		//FileInputStream inStream = new FileInputStream("db.properties");
+		
+		InputStream inStream = PropertiesTest.class.getResourceAsStream("/webapp/resource/db.properties");
+		
+		dbprop.load(inStream);
+		
+		String ClassName = dbprop.getProperty("oracle.ClassName");
+		String url = dbprop.getProperty("oracle.url");
+		String user = dbprop.getProperty("oracle.user");
+		String password = dbprop.getProperty("oracle.password");
+		
+		System.out.println("classname = " + ClassName);
+		System.out.println("url = "+ url);
+		System.out.println("user = " + user);
+		System.out.println("password = "+ password);
+
+	}
+
+}
